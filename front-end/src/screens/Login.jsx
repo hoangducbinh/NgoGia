2c47557c768dc14ee341424e2319b804f4b2f9a0
Hiep Thai <hiepproso01@gmail.com>
2024-08-08 16:23:21 +0700

    update img background

--- front-end/src/screens/Login.jsx
+++ front-end/src/screens/Login.jsx
@@ -1,27 +1,61 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "../style/login.css";
-const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    
-    return (
-        <div class="wrapper">
-            <div className="logo">
-                <img src="src/assets/img/logo.png" alt="Logo" />
-            </div>
-            <form action="#">
-                <h2>WELCOME BACK</h2>
-                <div class="input-field">
-                    <input type="text" required value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <label>Tài khoản</label>
-                </div>
-                <div class="input-field">
-                    <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <label>Mật khẩu</label>
-                </div>
-                <button type="submit" onClick={handleLogin} className="btn-submit">Log In</button>
-            </form>
+
+const Login = ({ setIsLoggedIn }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Tài khoản giả
+    const fakeUsername = 'hiep123';
+    const fakePassword = '123';
+
+    if (username === fakeUsername && password === fakePassword) {
+      setIsLoggedIn(true);
+      alert('Đăng nhập thành công!');
+      navigate('/home');
+    } else {
+      alert('Tài khoản hoặc mật khẩu không đúng!');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="logo">
+        <img src="src/assets/logo.png" alt="Logo" />
+      </div>
+      <form onSubmit={handleLogin}>
+        <h2>WELCOME BACK</h2>
+        <div className="input-field">
+          <input 
+            type="text" 
+            required 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+          <label>Tài khoản</label>
         </div>
-    )
-}
+        <div className="input-field">
+          <input 
+            type="password" 
+            required 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <label>Mật khẩu</label>
+        </div>
+        <button 
+          type="submit" 
+          className="btn-submit"
+        >
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default Login;
